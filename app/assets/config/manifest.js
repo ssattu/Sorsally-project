6934d42a4260645f310f7c60cefbd395c1a64980
Satyam Sharma <satyam@devohack.com>
2022-02-21 18:46:36 +0530

    Added about us page in Sorsally

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link_tree ../products
+//= link_tree ../about
 //= link slider.js
 //= link sign.js
 //= link gallery.js
