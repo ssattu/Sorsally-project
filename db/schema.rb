0c34e4570099c30e17c7d71d14b1ffc3de30a952
Satyam Sharma <satyam@devohack.com>
2022-02-24 18:59:18 +0530

    Update schema.eb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_23_084305) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_24_072729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.text "description"
+    t.string "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -41,6 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_23_084305) do
     t.string "state"
     t.string "city"
     t.string "contact"
+    t.string "login"
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
