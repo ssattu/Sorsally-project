b501ce4d221ec5412549933784ff1a46f39568ee
Satyam Sharma <satyam@devohack.com>
2022-02-24 17:30:31 +0530

    Fix manifest.json

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link_tree ../about
 //= link slider.js
 //= link sign.js
 //= link gallery.js
