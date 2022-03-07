660e387838515b2e936ed38d976bf92f5a87c226
Satyam Sharma <satyam@devohack.com>
2022-03-07 16:25:51 +0530

    Add seed file in sorsally

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,15 @@
 #
 #   movies = Movie.create([ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+	
+
+	Category.create!([{name: 'Tops & Blouses'}, {name: 'Shirts'}, {name: 'T-Shirts'}, {name: 'Hoodies'}, {name: 'Jackets'}, {name: 'Pants and Joggers'}, {name: 'Shorts'}, {name: 'Tracksuit Sets'}, {name: 'Skirts'}, {name: 'Dresses'}, {name: 'Jumpsuit & Rompers'}])
 
 
-# 10.times {
-	Product.create!(name:"T-shirt", description:"lorem_ipsum", price:130.50, gender:"men", category_id:3, images:"https://images.meesho.com/images/products/38929130/4qayg_512.jpg")
-# 	Product.create!(name:"Hoddies", description:"lorem_ipsum", price:150.50, gender:"women", category_id:5, images:"https://images.meesho.com/images/products/38929130/4qayg_512.jpg")
-# }
+10.times {
+	Product.create!(name:"T-shirt", description:"lorem_ipsum", price:130.50, gender:"men", category_id:Category.find_by(name:"T-Shirts").id, quantity:50)
+	Product.create!(name:"Tops & Blouses", description:"lorem_ipsum", price:130.50, gender:"women", category_id:Category.find_by(name:"Tops & Blouses").id, quantity:50)
+	Product.create!(name:"Dresses", description:"lorem_ipsum", price:150.50, gender:"women", category_id:Category.find_by(name:"Dresses").id, quantity:50)
+	Product.create!(name:"Jackets", description:"lorem_ipsum", price:200.50, gender:"men", category_id:Category.find_by(name:"Jackets").id, quantity:50)
+	Product.create!(name:"T-Shirts", description:"lorem_ipsum", price:250.50, gender:"men", category_id:Category.find_by(name:"T-Shirts").id, quantity:50)
+}
