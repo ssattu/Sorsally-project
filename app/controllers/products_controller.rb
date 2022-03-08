92ccd660608f478e8cc52e18d33e920fe32ffe51
Satyam Sharma <satyam@devohack.com>
2022-03-08 18:36:21 +0530

    update signup sign in forn eye toggle

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,17 +3,11 @@ class ProductsController < ApplicationController
   before_action :permit_all_params
   before_action :set_categories, only: :index
 
-  def index
-    # debugger
 
-    # gender_ids
-    # categories_ids
-    # debugger
+  def index
     @product_filters = params if product_params.present?
-    # @products.
     if product_params.present?
       @products = Product.where( category_id: product_params[:categories] || Category.all.pluck(:id) ).where( gender: product_params[:gender] || [0, 1] ).paginate(page: (params[:page] || 1), per_page: 9)
-      # debugger
     else
       @products = Product.paginate(page: params[:page] || 1, per_page: 9)
     end
@@ -23,38 +17,11 @@ class ProductsController < ApplicationController
       format.js 
     end
 
-
-
-
-    # @products = Product.Product.paginate(page: params[:page], per_page: 9)
-    # if categories_ids.present? || gender_ids.present?
-    #   @products = Product.where(category_id: categories_ids).paginate(page: params[:page], per_page: 9) if categories_ids.present?
-    #   @products = Product.where(gender: gender_ids).paginate(page: params[:page], per_page: 9)if gender_ids.present? 
-    # elsif gender_ids.present?  && categories_ids.present?
-    #   @products = Product.where(category_id: categories_ids).where(gender: gender_ids).paginate(page: params[:page], per_page: 9) if (categories_ids.present? && gender_ids.present?)
-    # else 
-    # @products = Product.paginate(page: params[:page], per_page: 9)
-    #   # @products = Product.all.limit(9).offset(@page * 9)
-    # end
-    # debugger
-    # respond_to do |format|
-    #   format.html{}
-    #   format.json{ render json: @products.to_json }
-    #   format.js
-    # end
-
-    # @categories = Category.all
   end
 
     def create 
-      # debugger
       @product = Product.new(p_params)
-    # if @product.present?
-    #   @product.update(deleted:false)
-    # else
-    # debugger
       @product.save
-    # end
       redirect_to new_product_path 
 
     end
@@ -62,7 +29,7 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @product = Product.new()
+    current_user.is_admin? ? "#{@product = Product.new()}" : "#{redirect_to products_path}" 
   end
 
   private
