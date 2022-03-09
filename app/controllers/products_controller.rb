91df01cf4838919f78579c54302c12b599580b50
Satyam Sharma <satyam@devohack.com>
2022-03-09 18:00:42 +0530

    Add product edit and destroy function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,7 @@ class ProductsController < ApplicationController
 
   def index
     @product_filters = params if product_params.present?
+      # debugger
     if product_params.present?
       @products = Product.where( category_id: product_params[:categories] || Category.all.pluck(:id) ).where( gender: product_params[:gender] || [0, 1] ).paginate(page: (params[:page] || 1), per_page: 9)
     else
@@ -19,18 +20,51 @@ class ProductsController < ApplicationController
 
   end
 
-    def create 
-      @product = Product.new(p_params)
-      @product.save
-      redirect_to new_product_path 
-
-    end
-  def show
-  end
-
   def new
+    @action ='new'
     current_user.is_admin? ? "#{@product = Product.new()}" : "#{redirect_to products_path}" 
   end
+    
+  def create 
+    @product = Product.new(p_params)
+    # if@product.present?
+    #  @product.update(deleted:false)
+    # else
+     @product.save
+    # end
+    redirect_to new_product_path 
+
+  end
+
+  def edit
+    @action ='edit'
+    @product = Product.find(params[:id])
+
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    
+    if @product.update(b_params) 
+      redirect_to products_path, notice: "Product is Updated Successfully"
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    # debugger
+    # @product.update(deleted: true)
+
+    redirect_to products_path, notice: "Product is Deleted Successfully"
+  end
+
 
   private
   def permit_all_params
---
+++ db/migrate/20220309090931_add_delete_to_product.rb
@@ -0,0 +1,5 @@
+class AddDeleteToProduct < ActiveRecord::Migration[6.1]
+  def change
+    add_column :products, :deleted, :boolean, default:false
+  end
+end
