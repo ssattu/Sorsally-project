9304c8688c28fb4c3e37d6a86e47ad01909ed7aa
Satyam Sharma <satyam@devohack.com>
2022-03-21 10:30:06 +0530

    Fix user activate and deactivate action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "sorsallys#index"
+  get 'users/:id', to: "users#activate", as: "activate_user"
 
   devise_for :users, controllers: {sessions: "users/sessions" , registrations: "users/registrations"}
   resources :sorsallys
@@ -13,7 +14,6 @@ Rails.application.routes.draw do
   get 'categories/show'
   delete 'products/:id', to: "products#destroy", as: "delete_product"
   get 'products/:id/add_image', to: "products#add_image", as: "add_image"
-  get 'users/:id', to: "users#activate", as: "activate_user"
   get 'products', to: 'products#index'
 
 end
